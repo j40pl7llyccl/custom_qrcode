8b5a41cdc9f7ff1ba27d2c024962aafd3e4a048a
j40pl7llyccl <j40pl7lly@gmail.com>
2025-01-04 20:40:14 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,7 +5,7 @@ import qrcode
 
 app = Flask(__name__)
 
-@app.route('/generate_qrcode', methods=['GET', 'POST'])
+@app.route('/generate_qrcode', methods='GET')
 def generate_qrcode():
     """
     - GET:  直接回傳前端頁面 (index.html)，提供圖片上傳與文字輸入介面
